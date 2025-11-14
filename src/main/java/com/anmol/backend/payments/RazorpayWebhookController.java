0ca702355507f22c81aff652036ce3af57c8aca5
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2025-11-14 20:14:42 +0530

    adding razorpayWebhook

--- src/main/java/com/anmol/backend/payments/RazorpayWebhookController.java
+++ src/main/java/com/anmol/backend/payments/RazorpayWebhookController.java
@@ -1,4 +1,19 @@
 package com.anmol.backend.payments;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/webhooks/razorpay")
 public class RazorpayWebhookController {
+
+    @Value("${razorpay.webhook_secret}")
+    private String webhookSecret;
+
+    @PostMapping
+    public ResponseEntity<Void> onEvent(HttpServletRequest req, @RequestHeader("X-Razorpay-Signature") String signature) throws Exception {
+
+    }
 }
