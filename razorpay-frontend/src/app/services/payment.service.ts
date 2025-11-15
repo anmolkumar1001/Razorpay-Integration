de8415e801a292fefd79d8f4911fc7ff5bdf9815
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2025-11-15 22:18:53 +0530

    payment service

---
+++ razorpay-frontend/src/app/services/payment.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PaymentService {
+
+    private apiUrl = 'http://localhost:8080/api/payments';
+
+    constructor(private http: HttpClient) {}
+
+    createOrder(amount: number) {
+        const payload= {
+            amount: amount,
+            currency: 'INR',
+            receipt: 'rcpt_' + new Date().getTime()
+        };
+        return this.http.post<any>(`${this.apiUrl}/create-order`, payload);
+    }
+
+    verifyPayment(data: any) {
+        return this.http.post(`${this.apiUrl}/verify`, data);
+    }
+}
